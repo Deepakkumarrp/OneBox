3841ce2bfc61d3f68df0835b5a82619720a880c0
Deepakkumarrp <deepakkumarrp@gmail.com>
2024-04-03 02:41:34 +0530

    Dynamic data shown

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -78,8 +78,8 @@ function Login() {
                 localStorage.setItem("isLoggedIn", true);
                 dispatch({ type: LOGIN, payload: true });
               }}
-              to="https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=http://localhost:5173/"
-                // to="https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://one-box-brown.vercel.app/"
+              // to="https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=http://localhost:5173/"
+                to="https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://one-box-brown.vercel.app/"
             >
               Sign Up with Google
             </Link>
